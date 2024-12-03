ba1f8a6c2b087e0869e4c48598dd16ec977b7d24
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 21:48:40 +0000

    Remove unused includes

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include <Windows.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow)
 {

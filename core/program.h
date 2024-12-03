f57d7d410983919840bf0575aa011837aa6a963d
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:03:37 +0000

    Add program typedef struct

---
+++ core/program.h
@@ -0,0 +1,13 @@
+#ifndef __BLOK_PROGRAM_H
+#define __BLOK_PROGRAM_H
+
+#define STRICT 1
+#include <Windows.h>
+#include "args.h"
+
+typedef struct bkProgram
+{
+	bkArgs args;
+} bkProgram;
+
+#endif // !__BLOK_PROGRAM_H

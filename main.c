12efa572b9d4731bf8d7ec344a600a1a276dae8f
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:15:33 +0000

    Create program inst

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #define STRICT 1
 #include <Windows.h>
+#include "core/program.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow)
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+	_In_ INT nCmdShow)
 {
+	bkProgram program;
+	bkInit(&program, hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+	bkRun(&program);
+	bkFree(&program);
 	return 0;
 }

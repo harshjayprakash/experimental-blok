ea7008bdb92535fbbdb221d174e0a8213dbce5a1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:12:46 +0000

    Implement functions

--- core/program.c
+++ core/program.c
@@ -5,15 +5,20 @@ void bkInit(
 	LPWSTR pCommandLine, int showFlag
 )
 {
-
+	if (!pProgram) return;
+	pProgram->args.hInstance = hInstance;
+	pProgram->args.hPreviousInstance = hPreviousInstance;
+	pProgram->args.pCommandLine = pCommandLine;
+	pProgram->args.showFlag = showFlag;
 }
 
 void bkRun(bkProgram* pProgram)
 {
-
+	if (!pProgram) return;
+	(void)MessageBoxW(NULL, L"This is a test.", L"Blok", MB_OK);
 }
 
 void bkFree(bkProgram* pProgram)
 {
-
+	if (!pProgram) return;
 }

063122a34ed8bffeaeb5367debd177f6ee07b783
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:02:01 +0000

    Add args struct type

--- core/args.h
+++ core/args.h
@@ -1,4 +1,15 @@
 #ifndef __BLOK_ARGS_H
 #define __BLOK_ARGS_H
 
+#define STRICT 1
+#include <Windows.h>
+
+typedef struct bkArgs
+{
+	HINSTANCE hInstance;
+	HINSTANCE hPreviousInstance;
+	LPWSTR pCommandLine;
+	int showFlag;
+} bkArgs;
+
 #endif // !__BLOK_ARGS_H

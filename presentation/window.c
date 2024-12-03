484b79d8d266bd00e6795ca167831483834a6376
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:24:44 +0000

    Add window procedure

--- presentation/window.c
+++ presentation/window.c
@@ -1,5 +1,11 @@
 #include "window.h"
 
+static LRESULT CALLBACK __bkWindowProcedure(
+	HWND hWindow, UINT messageId, WPARAM wParam, LPARAM lParam)
+{
+	return (LRESULT)0;
+}
+
 void bkWindowInit(bkWindow* pWindow)
 {
 

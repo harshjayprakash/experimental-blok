eaa13cc29c0138303e58925139df11e8fc784ce1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-03 22:09:24 +0000

    Add function definitions

--- core/program.c
+++ core/program.c
@@ -1,1 +1,19 @@
 #include "program.h"
+
+void bkInit(
+	bkProgram* pProgram, HINSTANCE hInstance, HINSTANCE hPreviousInstance,
+	LPWSTR pCommandLine, int showFlag
+)
+{
+
+}
+
+void bkRun(bkProgram* pProgram)
+{
+
+}
+
+void bkFree(bkProgram* pProgram)
+{
+
+}

5a450318d16b587f7511277a0a7ff2ed8aad4d7e
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:32:51 +0000

    Create renderer typedef struct

--- presentation/gfx/renderer.h
+++ presentation/gfx/renderer.h
@@ -1,4 +1,12 @@
 #ifndef __BLOK_RENDERER_H
 #define __BLOK_RENDERER_H
 
+#include <Windows.h>
+#include "tools.h"
+
+typedef struct bkRenderer
+{
+	bkGraphicsTools tools;
+	RECT* pWindowArea;
+} bkRenderer;
 #endif // !__BLOK_RENDERER_H

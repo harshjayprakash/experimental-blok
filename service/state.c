88b3cbd61c2c124170d16684f760820d1a8c3a10
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:57:40 +0000

    Add function implementation

--- service/state.c
+++ service/state.c
@@ -1,1 +1,12 @@
 #include "state.h"
+
+void bkStateInit(bkState* state)
+{
+	if (!state) return;
+	state->user = (bkSquare){ { 0, 0 }, { 15, 15 } };
+}
+
+void bkStateFree(bkState* state)
+{
+	if (!state) return;
+}

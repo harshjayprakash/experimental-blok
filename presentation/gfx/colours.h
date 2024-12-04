15c0e27191e88d7cef228d4df256104790e2327a
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:30:45 +0000

    Add colour enum

--- presentation/gfx/colours.h
+++ presentation/gfx/colours.h
@@ -1,4 +1,13 @@
 #ifndef __BLOK_COLOURS_H
 #define __BLOK_COLOURS_H
 
+#include <Windows.h>
+
+typedef enum bkColours
+{
+	bkBlack = RGB(0, 0, 0),
+	bkWhite = RGB(255, 255, 255),
+	bkDarkBlue = RGB(0, 53, 89),
+} bkColours;
+
 #endif

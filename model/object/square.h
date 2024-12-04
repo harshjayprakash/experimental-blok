7b71078a3927c7cb8547fe0a5576696a21b86783
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:51:34 +0000

    Create square typedef struct

--- model/object/square.h
+++ model/object/square.h
@@ -1,4 +1,13 @@
 #ifndef __BLOK_SQUARE_H
 #define __BLOK_SQUARE_H
 
+#include "../util/position.h"
+#include "../util/size.h"
+
+typedef struct bkSquare
+{
+	bkPosition position;
+	bkSize size;
+} bkSquare;
+
 #endif // !__BLOK_SQUARE_H

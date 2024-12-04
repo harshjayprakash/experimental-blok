3f5d17055b00c695a744b2932172d26338af0450
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:31:47 +0000

    Add graphics tools typedef struct

--- presentation/gfx/tools.h
+++ presentation/gfx/tools.h
@@ -1,4 +1,11 @@
 #ifndef __BLOK_TOOLS_H
 #define __BLOK_TOOLS_H
 
+#define STRICT 1
+#include <Windows.h>
+
+typedef struct bkGraphicsTools
+{
+	HBRUSH darkBlueBrush;
+} bkGraphicsTools;
 #endif // !__BLOK_TOOLS_H

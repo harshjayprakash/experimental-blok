d33bbe94e0fa1d01f85801d18e5818b16a9ce805
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:43:13 +0000

    Create direction enum

---
+++ model/util/direction.h
@@ -0,0 +1,12 @@
+#ifndef __BLOK_DIRECTION_H
+#define __BLOK_DIRECTION_H
+
+typedef enum bkDirection 
+{
+	bkNorth = 12,
+	bkEast = 3,
+	bkSouth = 6,
+	bkWest = 9
+} bkDirection;
+
+#endif // !__BLOK_DIRECTION_H

b8e70ef370a574dddf99a1d28406434caa534c30
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-04 17:32:25 +0000

    Implement functions

--- presentation/gfx/tools.c
+++ presentation/gfx/tools.c
@@ -1,1 +1,14 @@
 #include "tools.h"
+#include "colours.h"
+
+void bkGraphicsToolsInit(bkGraphicsTools* pGfxTools)
+{
+	if (!pGfxTools) return;
+	pGfxTools->darkBlueBrush = CreateSolidBrush(bkDarkBlue);
+}
+
+void bkGraphicsToolsFree(bkGraphicsTools* pGfxTools)
+{
+	if (!pGfxTools) return;
+	if (pGfxTools->darkBlueBrush) (void)DeleteObject(pGfxTools->darkBlueBrush);
+}

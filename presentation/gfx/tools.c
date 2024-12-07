7ba64743978f9201194d90182c567c671b742a05
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-07 21:01:41 +0000

    Initialise and delete additional brushes

--- presentation/gfx/tools.c
+++ presentation/gfx/tools.c
@@ -5,10 +5,16 @@ void bkGraphicsToolsInit(bkGraphicsTools* pGfxTools)
 {
 	if (!pGfxTools) return;
 	pGfxTools->darkBlueBrush = CreateSolidBrush(bkDarkBlue);
+	pGfxTools->aquaBrush = CreateSolidBrush(bkAqua);
+	pGfxTools->blackBrush = CreateSolidBrush(bkBlack);
+	pGfxTools->whiteBrush = CreateSolidBrush(bkWhite);
 }
 
 void bkGraphicsToolsFree(bkGraphicsTools* pGfxTools)
 {
 	if (!pGfxTools) return;
 	if (pGfxTools->darkBlueBrush) (void)DeleteObject(pGfxTools->darkBlueBrush);
+	if (pGfxTools->aquaBrush) (void)DeleteObject(pGfxTools->aquaBrush);
+	if (pGfxTools->blackBrush) (void)DeleteObject(pGfxTools->blackBrush);
+	if (pGfxTools->whiteBrush) (void)DeleteObject(pGfxTools->whiteBrush);
 }

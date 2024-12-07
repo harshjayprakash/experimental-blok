999df0e32b857ab8b29b3403132372b6464de62e
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-07 21:00:58 +0000

    Add blok rendering

--- presentation/gfx/renderer.c
+++ presentation/gfx/renderer.c
@@ -10,6 +10,14 @@ void bkRendererPaint(bkRenderer* pRenderer, bkState* state, HDC hDeviceContext, 
 {
 	if (!pRenderer || !state) return;
 	(void)FillRect(hDeviceContext, &kWindowArea, pRenderer->tools.darkBlueBrush);
+
+	RECT user = {
+		state->user.position.x,
+		state->user.position.y,
+		state->user.position.x + state->user.size.width,
+		state->user.position.y + state->user.size.height
+	};
+	(void)FillRect(hDeviceContext, &user, pRenderer->tools.aquaBrush);
 }
 
 void bkRendererFree(bkRenderer* pRenderer)

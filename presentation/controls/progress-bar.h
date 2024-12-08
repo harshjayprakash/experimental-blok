e771b3711993d1e842997280111f5088cc6a652c
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-08 15:24:56 +0000

    Create progress bar control typedef struct

--- presentation/controls/progress-bar.h
+++ presentation/controls/progress-bar.h
@@ -1,4 +1,19 @@
 #ifndef __BLOK_PROGRESSBAR_H
 #define __BLOK_PROGRESSBAR_H
 
+#include <Windows.h>
+
+typedef struct bkProgressBarControl
+{
+	WCHAR text[MAX_PATH];
+	SIZE_T maxTextSize;
+	COORD textPosition;
+	RECT barOutline;
+	RECT bar;
+	RECT region;
+	int barMin;
+	int barMax;
+	int barSpan;
+} bkProgressBarControl;
+
 #endif // !__BLOK_PROGRESSBAR_H

3043a46861f0d3002be0adae5ddee10240945c8c
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-08 15:25:11 +0000

    Create text control typedef struct

--- presentation/controls/text.h
+++ presentation/controls/text.h
@@ -1,4 +1,15 @@
 #ifndef __BLOK_TEXT_H
 #define __BLOK_TEXT_H
 
+#define STRICT 1
+#include <Windows.h>
+
+typedef struct bkTextControl
+{
+	WCHAR text[MAX_PATH];
+	SIZE_T max;
+	COORD position;
+	RECT area;
+} bkTextControl;
+
 #endif // !__BLOK_TEXT_H

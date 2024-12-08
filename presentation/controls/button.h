752b1c1458938eba63313aa86fb887cc0df51af3
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-08 15:24:42 +0000

    Create button control typedef struct

--- presentation/controls/button.h
+++ presentation/controls/button.h
@@ -1,4 +1,14 @@
 #ifndef __BLOK_BUTTON_H
 #define __BLOK_BUTTON_H
 
+#include <Windows.h>
+
+typedef struct bkButtonControl
+{
+	WCHAR text[MAX_PATH];
+	SIZE_T max;
+	COORD alignment;
+	RECT region;
+} bkButtonControl;
+
 #endif // !__BLOK_BUTTON_H

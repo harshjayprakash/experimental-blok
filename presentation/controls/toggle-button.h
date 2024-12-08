87f011c8fbe4771dca6dc2057bb287fc4ea19352
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-08 15:25:37 +0000

    Create toggle button control typedef struct

--- presentation/controls/toggle-button.h
+++ presentation/controls/toggle-button.h
@@ -1,4 +1,18 @@
 #ifndef __BLOK_TOGGLEBUTTON_H
 #define __BLOK_TOGGLEBUTTON_H
 
+#define STRICT 1
+#include <Windows.h>
+
+typedef struct bkToggleButtonControl
+{
+	WCHAR text[MAX_PATH];
+	SIZE_T max;
+	COORD textPosition;
+	RECT wholeArea;
+	RECT buttonArea;
+	RECT selectedArea;
+	int selected;
+} bkToggleButtonControl;
+
 #endif // !__BLOK_TOGGLEBUTTON_H

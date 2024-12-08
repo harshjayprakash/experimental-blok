2c650d4b3bb00bb6399c2843507de40982932a99
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-12-08 15:23:19 +0000

    Add function signatures

--- presentation/components/canvas.h
+++ presentation/components/canvas.h
@@ -5,4 +5,11 @@ typedef struct bkCanvas
 {
 	int locked;
 } bkCanvas;
+
+void bkComponentCanvasInit(bkCanvas* pCanvas);
+
+void bkComponentCanvasRender(bkCanvas* pCanvas);
+
+void bkComponentCanvasFree(bkCanvas* pCanvas);
+
 #endif // !__BLOK_CANVAS_H

55149260a4abbd2a6f4b652b50c3add856f7f47b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-05-22 19:03:17 +0100

    docs(direction): add doxygen documentation

--- src/model/direction.c
+++ src/model/direction.c
@@ -1,3 +1,9 @@
+/**
+ * \file DIRECTION.C
+ * \date 22-05-2025
+ * \brief Implementation of the direction to vector routine.
+ */
+
 #include "direction.h"
 
 VectorII blokDirectionToVector(
--- src/model/direction.h
+++ src/model/direction.h
@@ -1,8 +1,21 @@
+/**
+ * \file DIRECTION.H
+ * \date 22-05-2025
+ * \brief Provides the direction enumeration and convert to vector routine.
+ */
+
 #ifndef _BLOK_DIRECTION_H_
 #define _BLOK_DIRECTION_H_
 
 #include "vector.h"
 
+/**
+ * \brief Direction.
+ * 
+ * \details
+ * Denotes the directions of the compass rose, and values of the clock. An unset
+ * direction has been provided as a default state.
+ */
 typedef enum _Direction {
     BLOK_DIRECTION_UNSET = 0,
     BLOK_DIRECTION_NORTH = 12,
@@ -11,6 +24,16 @@ typedef enum _Direction {
     BLOK_DIRECTION_WEST = 9,
 } Direction;
 
+/**
+ * \brief Direction to Vector.
+ * 
+ * \details
+ * Converts the direction to a two dimensional vector by a magnitude interval of -1 and
+ * 1. An unset or invalid direction will return a null vector {0, 0}.
+ * 
+ * \param[in] direction   The direction to be converted.
+ * \return                The vector denoting direction.
+ */
 VectorII blokDirectionToVector(const Direction direction);
 
 #endif // _BLOK_DIRECTION_H_

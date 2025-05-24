78cabe8bcc43ec2fe51eafd23b862f9f2987018a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-05-24 11:37:48 +0100

    docs(square): add doxygen documentation

--- src/model/square.h
+++ src/model/square.h
@@ -1,11 +1,23 @@
+/**
+ * \file SQUARE.H
+ * \date 23-05-2023
+ * \brief Provides the square structure.
+ */
+
 #ifndef _BLOK_SQUARE_H_
 #define _BLOK_SQUARE_H_
 
 #include "vector.h"
 
+/**
+ * \brief Square.
+ * 
+ * \details
+ * Represents the square object.
+ */
 typedef struct _Square {
-    VectorII position;
-    VectorII size;
+    VectorII position;   /**< The square's position. */
+    VectorII size;       /**< The square size. */
 } Square;
 
 #endif // _BLOK_SQUARE_H_

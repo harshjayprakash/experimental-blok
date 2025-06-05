1e360543d3fe6040c7288be11f614e11699c23be
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-05 20:43:55 +0100

    style: format

--- source/blok/gdi/theme.c
+++ source/blok/gdi/theme.c
@@ -11,17 +11,37 @@ int blokColoursSet(
         return 0;
     }
 
-    pThemePalette->baseBackground = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_100, BLOK_COLOUR_BASE_900);
-    pThemePalette->baseBackgroundFaded = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_200, BLOK_COLOUR_BASE_800);
-    pThemePalette->baseBackgroundMedium = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_500, BLOK_COLOUR_BASE_400);
-    pThemePalette->baseForeground = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_950, BLOK_COLOUR_BASE_050);
-    pThemePalette->baseBorder = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_500, BLOK_COLOUR_BASE_400);
-    pThemePalette->baseBorderFaded = BLOK_LIGHT_DARK(BLOK_COLOUR_BASE_300, BLOK_COLOUR_BASE_700);
+    pThemePalette->baseBackground = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_100,
+        BLOK_COLOUR_BASE_900);
+    pThemePalette->baseBackgroundFaded = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_200,
+        BLOK_COLOUR_BASE_800);
+    pThemePalette->baseBackgroundMedium = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_500,
+        BLOK_COLOUR_BASE_400);
+    pThemePalette->baseForeground = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_950,
+        BLOK_COLOUR_BASE_050);
+    pThemePalette->baseBorder = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_500,
+        BLOK_COLOUR_BASE_400);
+    pThemePalette->baseBorderFaded = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_BASE_300,
+        BLOK_COLOUR_BASE_700);
     
-    pThemePalette->primaryBackground = BLOK_LIGHT_DARK(BLOK_COLOUR_PRIMARY_700, BLOK_COLOUR_PRIMARY_400);
-    pThemePalette->primaryBackgroundFaded = BLOK_LIGHT_DARK(BLOK_COLOUR_PRIMARY_100, BLOK_COLOUR_PRIMARY_900);
-    pThemePalette->primaryForeground = BLOK_LIGHT_DARK(BLOK_COLOUR_PRIMARY_900, BLOK_COLOUR_PRIMARY_100);
-    pThemePalette->primaryBorder = BLOK_LIGHT_DARK(BLOK_COLOUR_PRIMARY_700, BLOK_COLOUR_PRIMARY_400);
+    pThemePalette->primaryBackground = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_PRIMARY_700,
+        BLOK_COLOUR_PRIMARY_400);
+    pThemePalette->primaryBackgroundFaded = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_PRIMARY_100,
+        BLOK_COLOUR_PRIMARY_900);
+    pThemePalette->primaryForeground = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_PRIMARY_900,
+        BLOK_COLOUR_PRIMARY_100);
+    pThemePalette->primaryBorder = BLOK_LIGHT_DARK(
+        BLOK_COLOUR_PRIMARY_700,
+        BLOK_COLOUR_PRIMARY_400);
 
     return 0;
 }

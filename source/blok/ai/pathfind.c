e13a160c02bcbfaa3ea9b955d2dd39f97d1c7547
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-06-08 17:14:06 +0100

    refactor: remove function

--- source/blok/ai/pathfind.c
+++ source/blok/ai/pathfind.c
@@ -1,12 +1,1 @@
 #include "pathfind.h"
-#include <math.h>
-
-double blokPathFindCalculateHeuristic(
-    const TVector2 ep,
-    const TVector2 sp)
-{
-    double dx = pow(ep.x - sp.x, 2);
-    double dy = pow(ep.y - sp.y, 2);
-
-    return sqrt(dx + dy);
-}
--- source/blok/ai/pathfind.h
+++ source/blok/ai/pathfind.h
@@ -1,8 +1,4 @@
 #ifndef _BLOK_PATHFIND_H_
 #define _BLOK_PATHFIND_H_
 
-#include "../model/vector.h"
-
-double blokPathFindCalculateHeuristic(const TVector2 ep, const TVector2 sp);
-
 #endif /* _BLOK_PATHFIND_H_ */

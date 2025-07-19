c6bc548056ea7af14288f24a1d2b30bba083200e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-19 19:12:21 +0100

    refactor: rename namespace folder
    - `blok` -> `standstone`

--- source/main.c
+++ source/main.c
@@ -1,6 +1,30 @@
-#include "blok/lifecycle.h"
+/**
+ * @file main.c
+ * @brief Entrypoint.
+ * @author harshjayprakash
+ * @date 2025-07-19
+ ****************************************************************************************/
+
+#include "standstone/lifecycle.h"
 #include <windows.h>
 
+/**
+ * @brief Windows application entrypoint.
+ * 
+ * @details
+ * Attempts to perform an instance check. If there is no other instance running, the
+ * program will continue to execute, else it will exit.
+ * 
+ * @param[in] hInstance     The handle to the application instance.
+ * @param[in] hPrevInstance Always NULL.
+ * @param[in] lpCmdLine     The zero-terminated string of command line arguments.
+ * @param[in] nShowCmd      The flag on how the window will be displayed.
+ * @return Zero for success, non-zero for failure.
+ * 
+ * @remarks
+ * - Application start-up can be prevented by using the same mutex string.
+ * - Unicode-only. Wide character version functions are specifically used.
+ */
 int WINAPI wWinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -10,6 +34,8 @@ int WINAPI wWinMain(
     (void)hPrevInstance;
 
     HANDLE hInstanceMutex = CreateMutexW(NULL, TRUE, L"BlokInstance");
+
+    OutputDebugStringW(L"This is a test");
 
     if (hInstanceMutex == NULL) {
         (void)MessageBoxW(

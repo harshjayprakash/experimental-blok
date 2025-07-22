86be9eee71184cec6d4f4f429549292b6a9e61a2
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-22 21:26:35 +0100

    docs(model.square): add doxygen

--- source/standstone/model/square.h
+++ source/standstone/model/square.h
@@ -1,11 +1,28 @@
+/**
+ * @file square.h
+ * @brief Square Declarations.
+ * @author harshjayprakash
+ * @date 2025-07-22
+ ****************************************************************************************/
+
 #ifndef ST_SQUARE_H
 #define ST_SQUARE_H
 
 #include "vector.h"
 
+/**
+ * @brief Square Entity.
+ */
 typedef struct _Square
 {
+    /**
+     * @brief Position of the square.
+     */
     TVector2 position;
+
+    /**
+     * @brief Size of the square.
+     */
     TVector2 size;
 } TSquare;
 

82f69ec95b581e421ddd2db202a8bec132526ade
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-07-31 12:30:16 +0100

    build: update toggle logic
    - create macro
    - cache values
    - add features

--- config/FeatureToggles.cmake
+++ config/FeatureToggles.cmake
@@ -2,12 +2,24 @@
 # Please do not touch the if statements in this file. These checks ensure that the
 # features for basic functionality are still available.
 
-# Feature: Single Instance
-# - Uses a Mutex object to determine if another instance of the application is running.
-# - @see CreateMutexW
-option(__ST_FEATURE_SINGLE_INSTANCE ON)
+macro(ST_FEATURE _MACRO _TOGGLE _COMMENT)
+	set(${_MACRO} ${_TOGGLE} CACHE BOOL "${_COMMENT}" FORCE)
+	if(${_MACRO})
+		add_compile_definitions(${_MACRO})
+		message(STATUS "[Sandstone Features] -- ${_MACRO}: ${_COMMENT}")
+	endif()
+endmacro()
 
-if(__ST_FEATURE_SINGLE_INSTANCE)
-	add_compile_definitions(__ST_FEATURE_SINGLE_INSTANCE)
-	message(STATUS "[Sandstone Feature Toggles] Single Instance is Enabled")
-endif()
+ST_FEATURE(STF_NO_SINGLE_INSTANCE  ON   "Skip Single Instance Checks")
+ST_FEATURE(STF_NO_MAZE_GENERATION  ON   "Disables Procedurally Generating Mazes")
+ST_FEATURE(STF_NO_INPUT_LEGACY     OFF  "Ignores Box Movement via Arrow Keys")
+ST_FEATURE(STF_NO_INPUT_CONTEXTUAL OFF  "Ignores Box Movement via WASD Keys")
+ST_FEATURE(STF_NO_CANVAS_LOCK      OFF  "Disables the Canvas Lock")
+ST_FEATURE(STF_NO_CANVAS_BOUNDS    ON   "Disables Canvas Size Constraints for the Box")
+ST_FEATURE(STF_NO_CANVAS_GRID      OFF  "Disables Canvas Grid Toggle")
+ST_FEATURE(STF_NO_INPUT_DRAG_CLICK OFF  "Disables Canvas Drag Click Interaction")
+ST_FEATURE(STF_NO_CANVAS_LCLICK    OFF  "Disables Wall Creation by Left Click")
+ST_FEATURE(STF_NO_CANVAS_RCLICK    OFF  "Disables Wall Removal by Right Click")
+ST_FEATURE(STF_NO_WALLS            OFF  "Disables the Walls")
+ST_FEATURE(STF_NO_BOX              OFF  "Disables the Main Box Entity")
+ST_FEATURE(STF_NO_INTERFACE        OFF  "Disables the Interface Elements")

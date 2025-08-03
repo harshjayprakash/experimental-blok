0f455e9f926ceb1096fd0818b6326b235e9c82a6
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-03 21:45:05 +0100

    style: format

--- source/sandstone/gdi/theme.c
+++ source/sandstone/gdi/theme.c
@@ -1,55 +1,39 @@
 /**
- * @file theme.c
- * @brief Theme Implementation
+ * @file   theme.c
+ * @brief  Theme Implementation
  * @author harshjayprakash
- * @date 2025-07-20
+ * @date   2025-07-20
  ****************************************************************************************/
 
 #include "theme.h"
 
-#define ST_LIGHT_DARK(light, dark)                                                       \
-    (theme == ST_THEME_LIGHT) ? light : dark
+#define ST_LIGHT_DARK(light, dark) (theme == ST_THEME_LIGHT) ? light : dark
 
-int stThemeApply(
-    TThemePalette *pThemePalette,
-    const TTheme theme)
+int stThemeApply(TThemePalette *pThemePalette, const TTheme theme)
 {
     if (pThemePalette == NULL)
     {
         return 0;
     }
 
-    pThemePalette->baseBackground = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_100,
-        ST_COLOUR_BASE_900);
-    pThemePalette->baseBackgroundFaded = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_200,
-        ST_COLOUR_BASE_800);
-    pThemePalette->baseBackgroundMedium = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_500,
-        ST_COLOUR_BASE_400);
-    pThemePalette->baseForeground = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_950,
-        ST_COLOUR_BASE_050);
-    pThemePalette->baseBorder = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_500,
-        ST_COLOUR_BASE_400);
-    pThemePalette->baseBorderFaded = ST_LIGHT_DARK(
-        ST_COLOUR_BASE_300,
-        ST_COLOUR_BASE_700);
-    
-    pThemePalette->primaryBackground = ST_LIGHT_DARK(
-        ST_COLOUR_PRIMARY_700,
-        ST_COLOUR_PRIMARY_400);
-    pThemePalette->primaryBackgroundFaded = ST_LIGHT_DARK(
-        ST_COLOUR_PRIMARY_100,
-        ST_COLOUR_PRIMARY_900);
-    pThemePalette->primaryForeground = ST_LIGHT_DARK(
-        ST_COLOUR_PRIMARY_900,
-        ST_COLOUR_PRIMARY_100);
-    pThemePalette->primaryBorder = ST_LIGHT_DARK(
-        ST_COLOUR_PRIMARY_700,
-        ST_COLOUR_PRIMARY_400);
+    pThemePalette->baseBackground = ST_LIGHT_DARK(ST_COLOUR_BASE_100, ST_COLOUR_BASE_900);
+    pThemePalette->baseBackgroundFaded =
+        ST_LIGHT_DARK(ST_COLOUR_BASE_200, ST_COLOUR_BASE_800);
+    pThemePalette->baseBackgroundMedium =
+        ST_LIGHT_DARK(ST_COLOUR_BASE_500, ST_COLOUR_BASE_400);
+    pThemePalette->baseForeground = ST_LIGHT_DARK(ST_COLOUR_BASE_950, ST_COLOUR_BASE_050);
+    pThemePalette->baseBorder = ST_LIGHT_DARK(ST_COLOUR_BASE_500, ST_COLOUR_BASE_400);
+    pThemePalette->baseBorderFaded =
+        ST_LIGHT_DARK(ST_COLOUR_BASE_300, ST_COLOUR_BASE_700);
+
+    pThemePalette->primaryBackground =
+        ST_LIGHT_DARK(ST_COLOUR_PRIMARY_700, ST_COLOUR_PRIMARY_400);
+    pThemePalette->primaryBackgroundFaded =
+        ST_LIGHT_DARK(ST_COLOUR_PRIMARY_100, ST_COLOUR_PRIMARY_900);
+    pThemePalette->primaryForeground =
+        ST_LIGHT_DARK(ST_COLOUR_PRIMARY_900, ST_COLOUR_PRIMARY_100);
+    pThemePalette->primaryBorder =
+        ST_LIGHT_DARK(ST_COLOUR_PRIMARY_700, ST_COLOUR_PRIMARY_400);
 
     return 1;
 }

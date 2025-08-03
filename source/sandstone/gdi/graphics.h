301ef290bebbe76a55ec32d13ddf2eb5106b81ec
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-03 21:33:35 +0100

    docs(gdi.graphics): update doxy

--- source/sandstone/gdi/graphics.h
+++ source/sandstone/gdi/graphics.h
@@ -1,82 +1,72 @@
 /**
- * @file graphics.h
- * @brief Graphics Declarations.
+ * @file   graphics.h
+ * @brief  Graphics Declarations.
  * @author harshjayprakash
- * @date 2025-07-20
+ * @date   2025-08-03
  ****************************************************************************************/
 
 #ifndef ST_GRAPHICS_H
 #define ST_GRAPHICS_H
 
-#include "theme.h"
 #include "rendertools.h"
+#include "theme.h"
 
 /**
  * @brief Graphics context.
- * 
+ *
  * @details
- *  Contains the theming and tools used to paint the user interface. This is managed via
- *  the stGraphicsInit and stGraphicsFree functions.
+ * This structure contains the theming and tools used to paint the user interface. This is
+ * managed via the `stGraphicsInit` and `stGraphicsFree` lifecycle functions.
+ *
+ * @remarks
+ * - The caller must not modify these values directly.
  */
 typedef struct _Graphics
 {
-    /**
-     * @brief The current application theme.
-     * 
-     * @details
-     *  Set by the parsed arguments on start-up.
-     */
-    TTheme currentTheme;
-
-    /**
-     * @brief The rendering tools.
-     * 
-     * @details
-     *  Provides the pens and brushes used to paint on the window. This type contains
-     *  its own lifetime functions.
-     */
-    TRenderTools renderTools;
-
-    /**
-     * @brief The theme palette.
-     * 
-     * @details
-     *  Provides the available colours based on the application theme.
-     */
-    TThemePalette palette;
+    TTheme currentTheme;      /**< Current application theme */
+    TRenderTools renderTools; /**< Render tools. */
+    TThemePalette palette;    /**< Theme colours. */
 } TGraphics;
 
 /**
- * @brief Initialise the graphics.
+ * @brief Initialise the graphics context.
  * 
  * @details
- *  Validates the given theme, and then proceeds to set the theme palette and initialise
- *  the render tools.
+ * This function validates the given theme, and then proceeds to apply the theme palette,
+ * and initialise the render tools.
  * 
- * @param[out] pGraphics A valid pointer to TGraphics.
- * @param[in]  theme     The theme to be set.
- * @return One on success, zero on failure.
+ * @param[out] pGraphics   Pointer to the graphics context.
+ * @param[in]  theme       Theme to be set.
+ * @return `1` for success, `0` on failure.
  * 
- * @remark
- *  An invalid theme will default to the dark theme as a fallback.
- * @remark
- *  Memory is allocated, the corresponding free function stGraphics Free must be called
- *  on clean up.
+ * @remarks
+ * - An invalid theme will default to the dark theme as a fallback.
+ * - Memory is allocated, the corresponding free function stGraphics Free must be called
+ *   on clean up.
  */
 int stGraphicsInit(TGraphics *pGraphics, const TTheme theme);
 
 /**
- * @brief Free the graphics.
- * 
+ * @brief Free the graphics context.
+ *
  * @details
- *  Provides a wrapper for cleaning up the render tools.
- * 
- * @param[out] pGraphics A valid pointer to TGraphics.
- * @return One on success, zero on failure.
+ * This function provides a wrapper for cleaning up the render tools.
+ *
+ * @param[out] pGraphics   Pointer to the graphics context.
+ * @return `1` for success, `0` on failure.
  */
 int stGraphicsFree(TGraphics *pGraphics);
 
-
+/**
+ * @brief Reload the graphics context.
+ * 
+ * @details
+ * This function switches the theme and reloads the graphics, internally calling the
+ * free and initialise lifecycle functions.
+ * 
+ * @param[out] pGraphics   Pointer to the graphics context.
+ * @return `1` for success, `0` on failure.
+ */
 int stGraphicsSwitchTheme(TGraphics *pGraphics);
 
 #endif /* ST_GRAPHICS_H */

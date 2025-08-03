16f550b499947b04364fbc7aedde5e3433baa6c8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-03 13:47:11 +0100

    docs(core.lifecycle): update doxy

--- source/sandstone/core/lifecycle.h
+++ source/sandstone/core/lifecycle.h
@@ -16,82 +16,75 @@
  * @brief Semantic exit codes.
  *
  * @details
- *  Represents specific application termination states returned from the lifecycle
- *  functions (stSetup and stEntry).
- * 
+ * Represents specific application termination states returned from the lifecycle
+ * functions (`stSetup` and `stEntry`).
+
+ * The exit codes are bit-shifted to prevent clashing with the exit code from
+ * `PostQuitMessage`.
+ *
  * @remark
- *  These exit codes are passed to `wWinMain` and provide diagnostics on potential
- *  errors, when initialising the application.
+ * - These exit codes are passed to `wWinMain` and provide diagnostics on potential
+ *   errors, when initialising the application.
  */
 typedef enum _ExitStatus
 {
-    /** @brief Application exited successfully with no errors. */
-    ST_EXIT_SUCCESS = (0x0 << 6),
-
-    /**
-     * @brief A required pointer was NULL during initialisation.
-     *
-     * @details
-     *  Returned from stSetup or stEntry, if any required arguments are NULL.
-     */
-    ST_EXIT_NULLPTR = (0x1 << 6),
-
-    /** @brief The application object state module initialisation failed. */
-    ST_EXIT_STATE_ERROR = (0x2 << 6),
-
-    /** @brief The application graphics module initialisation failed. */
-    ST_EXIT_GRAPHICS_ERROR = (0x3 << 6),
-
-    /** @brief The application viewport module initialisation failed. */
-    ST_EXIT_VIEWPORT_ERROR = (0x4 << 6),
-
-    ST_EXIT_CLEANUP_ERROR = (0x5 << 6),
-
-    ST_EXIT_INSTANCE_RUNNING = (0x6 << 6),
-
-    ST_EXIT_INSTANCE_CHECK_ERROR = (0x7 << 6),
+    ST_EXIT_SUCCESS = (0x0 << 6), /**< Application exited successfully with no errors. */
+    ST_EXIT_NULLPTR =
+        (0x1 << 6), /**< A required pointer was NULL during initialisation. */
+    ST_EXIT_STATE_ERROR = (0x2 << 6), /**< Object state module initialisation failed. */
+    ST_EXIT_GRAPHICS_ERROR = (0x3 << 6),   /**< Graphics module initialisation failed. */
+    ST_EXIT_VIEWPORT_ERROR = (0x4 << 6),   /**< Viewport module initialisation failed. */
+    ST_EXIT_CLEANUP_ERROR = (0x5 << 6),    /**< Clean-up operation failed. */
+    ST_EXIT_INSTANCE_RUNNING = (0x6 << 6), /**< Existing instance is running. */
+    ST_EXIT_INSTANCE_CHECK_ERROR = (0x7 << 6), /**< Instance mutex creation failed. */
 } TExitStatus;
 
 /**
- * @brief Setup Sandstone.
+ * @brief Setup.
  *
  * @details
- *  Zero initialises the application context and then proceeds to parse the arguments.
- *  The application instance and show flag is saved within the context.
- * 
- * @param[in] hInstance    A valid handle to the application instance.
- * @param[in] pCommandLine The command line arguments.
- * @param[in] showFlag     How the window should be displayed.
+ * This function zero initialises the application context and then proceeds to parse the
+ * arguments. The provided instance handle and show flag are saved within the context.
+ *
+ * @param[in] hInstance      Handle to the application instance.
+ * @param[in] pCommandLine   Pointer to the command-line.
+ * @param[in] showFlag       Initial window display state.
  * @return Zero for success, non-zero for failure. For specific return values, please
  *         refer to TExitStatus.
+ *
+ * @remarks
+ * - This function must be called from wWinMain as an entrypoint into the Sandstone
+ *   application. stEntry is called internally.
+ * - The showFlag value is not checked and will be passed to ShowWindow.
  * 
- * @remark
- *  This function must be called from wWinMain as an entrypoint into the Sandstone
- *  application. stEntry is called internally.
- * @remark
- *  The showFlag value is not checked and will be passed to ShowWindow.
+ * @see stEntry
+ * @see TContext
+ * @see stArgsParse
+ * @see TParsedArgs
  */
 int stSetup(HINSTANCE hInstance, LPWSTR pCommandLine, int showFlag);
 
 /**
- * @brief Sandstone Entry.
+ * @brief Entry.
  *
  * @details
- *  Initialises all the contextual modules, based on the provided parsed arguments. All
- *  memory is freed when exiting this function.
- * 
- * @param[in, opt] pContext A valid pointer to the application context.
- * @param[in]      pArgs    A valid pointer to the TParsedArgs structure.
+ * This function initialises all the contextual modules, based on the provided parsed
+ * arguments. All memory is freed when exiting this function.
+ *
+ * @param[in, opt] pContext   Pointer to application context.
+ * @param[in]      pArgs      Pointer to parsed arguments.
  * @return Zero for success, non-zero for failure. For specific return values, please
  *         refer to TExitStatus.
+ *
+ * @remarks
+ * - This function should not be called by the user. It is called by stSetup,
+ *   providing the valid arguments, after setup. An attempt to call this function may
+ *   lead to undefined behaviour.
+ * - If any of the initialisation of modules fails, the function will exit, returning
+ *   the appropriate exit code (passed to wWinMain). Any memory allocated is cleaned up.
  * 
- * @remark
- *  This function should not be called by the user. It is called by stSetup,
- *  providing the valid arguments, after setup. An attempt to call this function may lead
- *  to undefined behaviour.
- * @remark
- *  If any of the initialisation of modules fails, the function will exit, returning the
- *  appropriate exit code (passed to wWinMain). Any memory allocated is cleaned up.
+ * @see TContext
+ * @see TParsedArgs
  */
 int stEntry(TContext *pContext, TParsedArgs *pArgs);
 

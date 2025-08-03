2b53bffe00da946b7b34fa728396c45758f062d5
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-08-03 21:44:16 +0100

    docs(gdi.rendertools): update doxy

--- source/sandstone/gdi/rendertools.h
+++ source/sandstone/gdi/rendertools.h
@@ -1,8 +1,8 @@
 /**
- * @file rendertools.h
- * @brief Render Tools Declarations.
+ * @file   rendertools.h
+ * @brief  Render Tools Declarations.
  * @author harshjayprakash
- * @date 2025-07-20
+ * @date   2025-07-20
  ****************************************************************************************/
 
 #ifndef ST_RENDERTOOLS_H
@@ -13,19 +13,20 @@
 
 /**
  * @brief Rendering tools.
- * 
+ *
  * @details
- *  Contains all the rendering tools required for painting the user interface. The default
- *  configuration for HBRUSH is creating a solid brush based on the theme palette. HPEN
- *  follows the same principle with the PS_SOLID and 1 as the thickness.
+ * This function contains all the rendering tools required for painting the user
+ * interface. The default configuration for HBRUSH is creating a solid brush based on the
+ * theme palette. HPEN follows the same principle with the PS_SOLID and 1 as the
+ * thickness.
  */
 typedef struct _RenderTools
 {
     /**
      * @brief Brushes.
-     * 
+     *
      * @details
-     *  Provides the brushes, named semantically based on use.
+     * This structure provides the brushes, named semantically based on use.
      */
     struct _Brushes
     {
@@ -38,9 +39,9 @@ typedef struct _RenderTools
 
     /**
      * @brief Pens.
-     * 
+     *
      * @details
-     *  Provides the pens, named semantically based on use.
+     * This structure provides the pens, named semantically based on use.
      */
     struct _Pens
     {
@@ -54,38 +55,36 @@ typedef struct _RenderTools
 
 /**
  * @brief Initialise the render tools.
- * 
+ *
  * @details
- *  Attempts to create all the brushes and pens.
- * 
- * @param[out] pTools   A valid pointer to TRenderTools.
- * @param[in]  pColours A valid pointer to TThemePalette.
- * @return One for success, zero for failure.
- * 
- * @remark
- *  The function assumes that theme palette has been set before this. A zeroed theme
- *  palette will likely result in a blank window.
- * @remark
- *  Memory is allocated, the corresponding stRenderToolsFree function must be called
- *  on clean-up.
- * @remark
- *  All allocated brushes and pens are checked against null to calculate the return
- *  value. If a single brush or pen creation fails, the function will return 1. In
- *  this case, the caller must clean-up, before re-attempting.
+ * This function attempts to create all the brushes and pens. 
+ *
+ * @param[out] pTools     Pointer to the render tools.
+ * @param[in]  pColours   Pointer to the theme palette.
+ * @return `1` for success, `0` for failure.
+ *
+ * @remarks
+ * - The function assumes that theme palette has been set before this. A zeroed theme
+ *   palette will likely result in a blank window.
+ * - Memory is allocated, the corresponding stRenderToolsFree function must be called
+ *   on clean-up.
+ * - All allocated brushes and pens are checked against null to calculate the return
+ *   value. If a single brush or pen creation fails, the function will return 1. In
+ *   this case, the caller must clean-up, before re-attempting.
  */
 int stRenderToolsInit(TRenderTools *pTools, const TThemePalette *pColours);
 
 /**
  * @brief Free the render tools.
- * 
+ *
  * @details
- *  Frees all the allocated brushes and pens.
- * 
- * @param[out] pTools A valid pointer to TRenderTools.
- * @return One for success, zero for failure.
- * 
+ * This function frees all the allocated brushes and pens.
+ *
+ * @param[out] pTools   Pointer to the render tools.
+ * @return `1` for success, `0` for failure.
+ *
  * @remark
- *  This function checks if the Gdi objects are valid before attempting to free them.
+ * - This function checks if the Gdi objects are valid before attempting to free them.
  */
 int stRenderToolsFree(TRenderTools *pTools);
 
